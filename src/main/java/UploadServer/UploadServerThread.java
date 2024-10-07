44ccfa9f5fce99c719b2b128f15c262289476116
Jimmy Kong <jimmyatwork368@gmail.com>
2024-10-07 10:42:52 -0700

    UploadServlet: Implemented the doGet method. Now users can get the form when they send GET request to localhost.

--- src/main/java/UploadServer/UploadServerThread.java
+++ src/main/java/UploadServer/UploadServerThread.java
@@ -16,6 +16,8 @@ public class UploadServerThread extends Thread {
          InputStream in = socket.getInputStream();
          OutputStream out = socket.getOutputStream();
 
+         HttpServlet httpServlet = new UploadServlet();
+
          /*
             Convert socket input stream into a BufferedInputStream
             so that we could use readLine() method to read the first line of incoming HTTP request
@@ -23,16 +25,35 @@ public class UploadServerThread extends Thread {
          BufferedReader reader = new BufferedReader(new InputStreamReader(in));
          String requestLine;
          String responseLine;
+         String httpMethod = "";
 
          // Read the first line of HTTP request
          requestLine = reader.readLine();
 
-         HttpServletRequest req = new HttpServletRequest(in);
-         OutputStream baos = new ByteArrayOutputStream();
-         HttpServletResponse res = new HttpServletResponse(baos); 
-         HttpServlet httpServlet = new UploadServlet();
+         // Parse HTTP method from the request line
+         if (requestLine != null && !requestLine.isEmpty()) {
+            String[] parts = requestLine.split(" ");
+            if (parts.length > 0) {
+               httpMethod = parts[0];
+               System.out.println(httpMethod);
+            } else {
+               System.out.println("Invalid request line");
+            }
+         } else {
+            System.out.println("No request line received");
+         }
+
+         if (httpMethod.equals("GET")) {
+            HttpServletRequest req = new HttpServletRequest(in);
+            HttpServletResponse res = new HttpServletResponse(out);
+
+            httpServlet.doGet(req, res);
+         }
+
+         // OutputStream baos = new ByteArrayOutputStream();
+         // HttpServletResponse res = new HttpServletResponse(baos);
          // httpServlet.doPost(req, res);
-         out.write(((ByteArrayOutputStream) baos).toByteArray());
+         // out.write(((ByteArrayOutputStream) baos).toByteArray());
          socket.close();
       } catch (Exception e) { e.printStackTrace(); }
    }
--- src/main/java/UploadServer/UploadServlet.java
+++ src/main/java/UploadServer/UploadServlet.java
@@ -3,6 +3,43 @@ package UploadServer;
 import java.io.*;
 import java.time.Clock;
 public class UploadServlet extends HttpServlet {
+   public static final String CRLF = "\r\n";
+
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+      try {
+         OutputStream out = response.getOutputStream();
+
+         String htmlContent = "<!DOCTYPE html>" +
+                 "<html>" +
+                 "<body>" +
+                 "<h2>HTML Forms</h2>" +
+                 "<form action=\"http://localhost:8999\" method=\"post\" enctype=\"multipart/form-data\">" +
+                 "Caption: <input type=\"text\" name=\"caption\" /><br /><br />" +
+                 "Date: <input type=\"date\" name=\"date\" /><br />" +
+                 "<input type=\"file\" name=\"fileName\" /><br /><br />" +
+                 "<input type=\"submit\" value=\"Submit\" />" +
+                 "</form>" +
+                 "</body>" +
+                 "</html>";
+
+         // Calculate the content length
+         int contentLength = htmlContent.getBytes("UTF-8").length;
+
+         // Set response headers
+         String headers = "HTTP/1.1 200 OK" + CRLF +
+                 "Content-Type: text/html; charset=UTF-8" + CRLF +
+                 "Content-Length: " + contentLength + CRLF +
+                 "Connection: close" + CRLF + CRLF;
+
+         String htmlMessage = headers + htmlContent + CRLF + CRLF;
+         out.write(htmlMessage.getBytes("UTF-8"));
+
+         System.out.println("User connects and gets the form.");
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+   }
+
    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
       try {
          InputStream in = request.getInputStream();   
