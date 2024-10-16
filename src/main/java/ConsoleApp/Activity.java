0df12ea780cb5ecf3c5c852fb35458e0ea5b406d
Henry Tan <henrytann00@gmail.com>
2024-10-16 01:06:30 -0700

    Added comment to locate where to change port number

--- src/main/java/ConsoleApp/Activity.java
+++ src/main/java/ConsoleApp/Activity.java
@@ -9,12 +9,14 @@ public class Activity {
    public Activity() {
    }
    public void onCreate() {
+      // CHANGE LOCALHOST FOR TYPE
+      // TO TOMCAT IF USING TOMCAT SERVER
       String uploadUrl = "http://localhost:8999/upload";
       String filePath = "src/main/java/ConsoleApp/AndroidLogo.png";
       // Gather the caption and date for the upload
       String caption = "TESTING final" +
               "";
-      String date = "2024-10-15"; // todays date
+      String date = "2024-10-15"; // Today's date
 
       UploadAsyncTask uploadAsyncTask = new UploadAsyncTask(uploadUrl, caption, date, filePath);
       uploadAsyncTask.execute();
